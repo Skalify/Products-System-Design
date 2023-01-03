9ff29b606f59d90a65d73aff98f35850a08fec20
Thomas Saldana <saldana.tom@gmail.com>
2023-01-03 15:25:52 -0800

    refactor clustering

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ if (cluster.isPrimary) {
 
   cluster.on('exit', (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`);
+    // cluster.fork();
   });
 } else {
   const app = require('./index.js');
