b743b721f78ad42e7238dedf7e57a83318a710d9
Thomas Saldana <saldana.tom@gmail.com>
2023-01-03 15:16:29 -0800

    setup up cluster

--- server/__stressTests__/styles.js
+++ server/__stressTests__/styles.js
@@ -5,19 +5,12 @@ import { Counter } from "k6/metrics";
 let ErrorCount = new Counter("errors");
 
 export const options = {
-  scenarios: {
-    constant_request_rate: {
-      executor: 'constant-arrival-rate',
-      rate: 1000,
-      timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
-      duration: '15s',
-      preAllocatedVUs: 20, // how large the initial pool of VUs would be
-      maxVUs: 50, // if the preAllocatedVUs are not enough, we can initialize more
-    },
-  },
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  stages: [{ duration: '30s', target: 1000 }],
   thresholds: {
-    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(100)<2000'], //http response should be less than 2000ms
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(100)<3000'],
   },
 };
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,24 @@
-const app = require('./index.js');
+const cluster = require('node:cluster');
+const http = require('node:http');
+const { cpus } = require('node:os');
+const process = require('node:process');
 
-app.listen(app.get('port'), () => {
-  console.log(`App is running on http://localhost:${app.get('port')}`);
-});
+const numCPUs = cpus().length;
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} running`);
+
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  const app = require('./index.js');
+  app.listen(app.get('port'), () => {
+    console.log(`App is running on http://localhost:${app.get('port')}`);
+  });
+  console.log(`Worker ${process.pid} started`);
+}
