352dea16ceab1fb7a573c044afb2d92397523909
Thomas Saldana <saldana.tom@gmail.com>
2023-01-02 16:16:21 -0800

    setup local stress tests for all routes

---
+++ server/__stressTests__/styles.js
@@ -0,0 +1,32 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { Counter } from "k6/metrics";
+
+let ErrorCount = new Counter("errors");
+
+// 1k RPS each route
+// Measure RPS, Latency, and Error Rate for each request.
+
+export const options = {
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 1000,
+      timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
+      duration: '15s',
+      preAllocatedVUs: 20, // how large the initial pool of VUs would be
+      maxVUs: 50, // if the preAllocatedVUs are not enough, we can initialize more
+    },
+  },
+};
+
+export default function() {
+  let res = http.get(`http://localhost:8080/products/1/styles`);
+  let success = check(res, {
+    "status is 200": r => r.status === 200
+  });
+  if (!success) {
+    ErrorCount.add(1);
+  }
+  sleep(2);
+}
